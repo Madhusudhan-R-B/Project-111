0e856b5793b1006e1742d0cc8943de6c7e9c811e
Madhusudhan-R-B <86708727+Madhusudhan-R-B@users.noreply.github.com>
2023-01-06 18:21:45 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+Webcam.set({
+    width:450,
+    height:350,
+    image_format:'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function snap(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="prediction_img" src="'+data_uri+'" style="width:450px; height:340px;">';
+    });
+}
+
+console.log("ml5 version", ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/1XP8K1Xkn/model.json", modelLoaded);
+
+function modelLoaded(){
+    console.log("Model Loaded!")
+}
+
+function predict(){
+    img = document.getElementById("prediction_img");
+    classifier.classify(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("prediction").innerHTML = "You have made the " + results[0].label + " Hand-Gesture!"
+    }
+}
